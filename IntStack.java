b3450bf753c894ac13aa131ab3d0ebe433ce748d
colette <c18ca@dalton.org>
2017-11-09 18:14:50 -0500

    Colette wrote stack

--- IntStack.java
+++ IntStack.java
@@ -1,15 +1,28 @@
+import java.util.Arrays;
 public class IntStack {
 	public static void main(String[] args) {
 		IntStack is = new IntStack();
 		System.out.println(is.isEmpty());
+		int [] test = new int [3];
+		test[0] = 1;
+		test[1] = 2;
+		test [2] = 3;
 		is.push(3);
 		is.push(4);
 		is.push(5);
 		System.out.println(is.isEmpty());
 		System.out.println(is.pop());
 		System.out.println(is.peek());
+		System.out.println("The size is " + is.size());
+		System.out.println("The value is " + is.peek(1));
+		System.out.println("The multiple list " + Arrays.toString(is.pop(2)));
+		//System.out.println("The new list " + is.push(test));
+		System.out.println("count " + is.count(3));
+		//System.out.println(is.print());
+        System.out.println("array " + Arrays.toString(is.toArray()));
+
 	}
-	
+
 	
 	int[] stack;
 	int top;
@@ -42,34 +55,53 @@ public class IntStack {
     make a new larger implementing array
     */
     private void resize() {
-
+        int capacity;
+    //Item[] temp =(Item[]) * new Object[capacity];
+    //int n = is.length;
+    //for(int i = 0; i <n; i++)
     }
 
     /*
     how large is the stack?
     */
     public int size() {
-		return 0;
+        int size = 0;
+        for(int i = top; i >=0; i--){
+            size++;
+        }
+        return size;
     }
 
     /*
     sort the contents of the stack
     */
     public void sort() {
-
+    if(!isEmpty()){
+        int i = pop();
+        sort();
+    }
     }
 
     /*
     print the Stack pretty-like
     */
     public void print() {
-
+    if(!isEmpty()){
+        for(Object a: stack){
+            System.out.println(a);
+        }
+    }
     }
 
     /*
     return the item depth distance from the top
     */
     public int peek(int depth) {
+    for(int i = top; i>=depth; i--){
+        if (i == depth){
+            return stack[i];
+        }
+    }
 			return 0;
     }
 
@@ -77,42 +109,68 @@ public class IntStack {
     return multiple items from the top in a new array
     */
     public int[] pop(int multiple) {
-			return null;
+        int[] multipleNew = new int[multiple];
+        int x = 0;
+        for(int i = top; i> top-multiple; i--){
+            multipleNew[x] =(stack[i]);
+            x++;
+        }
+			return multipleNew;
     }
 
     /*
     push multiple items onto the stack
     */
     public void push(int[] nums) {
-
+        int x = 0;
+        for(int i = 0; i<nums.length; i++)
+        if(top == stack.length) resize();
+        stack[top++] = nums[x];
+        x++;
     }
+
 
     /*
     how many [num]'s are n the stack?
     */
     public int count(int num) {
-			return 0;
+        int count = 0;
+        for(int i =top; i>=0; i--){
+            if (stack[i] == num){
+                count++;
+            }
+        }
+			return count;
     }
+
 
     /*
     remove depth items
     */
     public void dump(int depth) {
-
+    for(int i = top; i>=depth; i--){
+        pop();
+        }
     }
 
     /*
     return the contents of the stack as an array
     */
     public int[] toArray() {
-			return null;
+        int [] arrayVersion = new int [stack.length];
+        int x = 0;
+        for(int i =0; i < stack.length; i++){
+            arrayVersion[x] = stack[i];
+            x++;
+        }
+			return arrayVersion;
     }
 
     /*
     make the bottom of the stack the top
     */
     public void flip() {
-
+    
     }
 
     /*
